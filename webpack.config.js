0ddb6cc55310148e5d1b7effdbf5c33595685da2
Pokdee <loselife61@gmail.com>
2023-11-23 23:17:07 +0700

    Implement knight travailing algo and animation

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./script/script.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    // static: {
+    //   directory: path.join(__dirname, "public"),
+    // },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+  mode: "none",
+};
